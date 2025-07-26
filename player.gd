ebfbcaa5b009519be5c565cc264c171650c513f3
Joel Paine <apenguin777@gmail.com>
2025-07-25 23:11:54 -0400

    SELECTION POG

--- player.gd
+++ player.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+const moveSpeed=5000.0
+
+@export var Goal: Node = null
+
+func _ready() -> void:
+	$NavigationAgent2D.target_position=Goal.global_position
+
+func _physics_process(delta: float) -> void:
+	if(!$NavigationAgent2D.is_target_reached()):
+		var navPointDirection=to_local($NavigationAgent2D.get_next_path_position()).normalized()
+		velocity= navPointDirection*moveSpeed*delta
+		move_and_slide()
+
+
+
+func _on_timer_timeout() -> void:
+	if $NavigationAgent2D.target_position!=Goal.global_position:
+		$NavigationAgent2D.target_position=Goal.global_position
+
+	
---
+++ unit_selector.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var selecting: bool = false
+var dragStart: Vector2
+var selectBox: Rect2
+
+func _input(e: InputEvent) -> void:
+	if e is InputEventMouseButton and e.button_index==MOUSE_BUTTON_LEFT:
+		if(e.pressed):
+			selecting=true 
+			dragStart=e.position
+			
+		else:
+			selecting=false
+			queue_redraw()
+	elif selecting and e is InputEventMouseMotion:
+		var x_min=min(dragStart.x,e.position.x)
+		var y_min=min(dragStart.y,e.position.y)
+		selectBox=Rect2(x_min,y_min,max(dragStart.x,e.position.x)-x_min,max(dragStart.y,e.position.y)-y_min)
+		queue_redraw()
+
+func _draw():
+	
+	if not selecting: return
+	draw_rect(selectBox,Color("#00ff0066"))
+	draw_rect(selectBox,Color('#00ff00'),false,2.0)
