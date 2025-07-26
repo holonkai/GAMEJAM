410e596f3de6d62357714c89f38abf257633e79c
Joel Paine <apenguin777@gmail.com>
2025-07-25 23:56:57 -0400

    selection

--- player.gd
+++ player.gd
@@ -1,15 +1,28 @@
 extends CharacterBody2D
 const moveSpeed=5000.0
 
-@export var Goal: Node = null
 
+@export var Goal: Node = null
+@onready var cam: Camera2D=get_viewport().get_camera_2d()
 func _ready() -> void:
+	$Selector.hide()
 	$NavigationAgent2D.target_position=Goal.global_position
+
+func is_in_selection_box(box:Rect2):
+	return box.has_point(global_position)
+
+func select():
+	$Selector.show()
+	add_to_group("selected-units")
+
+func deselect():
+	$Selector.hide()
+	remove_from_group("selected-units")
 
 func _physics_process(delta: float) -> void:
 	if(!$NavigationAgent2D.is_target_reached()):
 		var navPointDirection=to_local($NavigationAgent2D.get_next_path_position()).normalized()
-		velocity= navPointDirection*moveSpeed*delta
+		#velocity= navPointDirection*moveSpeed*delta
 		move_and_slide()
 
 
--- unit_selector.gd
+++ unit_selector.gd
@@ -12,11 +12,15 @@ func _input(e: InputEvent) -> void:
 			
 		else:
 			selecting=false
+			if dragStart.is_equal_approx(e.position):
+				selectBox=Rect2(e.position,Vector2.ZERO)
+			update_selected_units()
 			queue_redraw()
 	elif selecting and e is InputEventMouseMotion:
 		var x_min=min(dragStart.x,e.position.x)
 		var y_min=min(dragStart.y,e.position.y)
 		selectBox=Rect2(x_min,y_min,max(dragStart.x,e.position.x)-x_min,max(dragStart.y,e.position.y)-y_min)
+		update_selected_units()
 		queue_redraw()
 
 func _draw():
@@ -24,3 +28,10 @@ func _draw():
 	if not selecting: return
 	draw_rect(selectBox,Color("#00ff0066"))
 	draw_rect(selectBox,Color('#00ff00'),false,2.0)
+
+func update_selected_units():
+	for unit in get_tree().get_nodes_in_group("selectableUnits"):
+		if(unit.is_in_selection_box(selectBox)):
+			unit.select()
+		else:
+			unit.deselect()
