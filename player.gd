9e19be7695baf0647dc27ad16f53b4a3d1e54857
Joel Paine <apenguin777@gmail.com>
2025-07-26 00:22:45 -0400

    Update player.gd

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 const moveSpeed=5000.0
-
+var clicked: bool=false
 
 @export var Goal: Node = null
 @onready var cam: Camera2D=get_viewport().get_camera_2d()
@@ -8,8 +8,17 @@ func _ready() -> void:
 	$Selector.hide()
 	$NavigationAgent2D.target_position=Goal.global_position
 
+func _input(event):
+	
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		clicked=false
+		var mouse_pos = get_viewport().get_mouse_position()
+		if $CollisionShape2D.shape is CircleShape2D:
+			if $CollisionShape2D.shape.get_rect().has_point(to_local(mouse_pos)):
+				clicked=true
+		
 func is_in_selection_box(box:Rect2):
-	return box.has_point(global_position)
+	return box.has_point(global_position)  or clicked
 
 func select():
 	$Selector.show()
