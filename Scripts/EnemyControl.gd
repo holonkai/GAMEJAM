e3320a46be3af62bfbc0cfc4a6c1f7daeb8eb7cb
Joel Paine <apenguin777@gmail.com>
2025-07-26 23:17:50 -0400

    fixed crashing bug with enemy control

--- Scripts/EnemyControl.gd
+++ Scripts/EnemyControl.gd
@@ -6,7 +6,7 @@ func targetSelection(pos :Vector2):
 	
 	var targets=get_tree().get_nodes_in_group("selectableUnits")
 	if(targets.size()<1):
-		return
+		return pos
 	var closest=targets[0].global_position
 	for target in targets:
 		if((pos-target.global_position).length()<(pos-closest).length()):
@@ -16,13 +16,14 @@ func targetSelection(pos :Vector2):
 func move_enemies():
 	#telling the enemies where to move
 	var units=get_tree().get_nodes_in_group("Enemies")
-
-	for i in units.size():
-		var position=targetSelection(units[i].global_position)
-		var path=NavigationServer2D.map_get_path(map, snap_to_map(units[i].global_position), snap_to_map(position),true)
-		#print(path)
-		units[i].move(path)
-		
+	if(units.size()>1):
+		for i in units.size():
+			var position=targetSelection(units[i].global_position)
+			print(units.size())
+			var path=NavigationServer2D.map_get_path(map, snap_to_map(units[i].global_position), snap_to_map(position),true)
+			#print(path)
+			units[i].move(path)
+			
 func snap_to_map(pos: Vector2):
 	#gets the closest map point
 	return NavigationServer2D.map_get_closest_point(map,pos)
