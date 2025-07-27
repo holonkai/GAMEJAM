a5a5b864d1676090e077a81e3748158471398171
rennic <160051984+holonkai@users.noreply.github.com>
2025-07-26 20:02:43 -0700

    buttons... and other

--- GAMEJAM-main/main-ui/main_ui.gd
+++ GAMEJAM-main/main-ui/main_ui.gd
@@ -15,48 +15,93 @@ func _ready():
 func update_gold_display():
 	gold_label.text = "Gold: %d" %gold
 
-		
+@onready var range_pop_up = $"background/LittleGuyEconomy/shopbox/range minion/RangePopup"
 func _on_range_minion_button_down() -> void:
 	var cost = 8
 	if gold >= cost:
 		gold -= cost 
 		update_gold_display()
-		print("RANGE MINION x1")
+		
+		range_pop_up.text = "RANGE MINION x1"
+		range_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		range_pop_up.visible = false
 	else:
-		print("UR POOR")
+		range_pop_up.text = "UR POOR"
+		range_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		range_pop_up.visible = false
+		
+	
+		
+	#	print("RANGE MINION x1")
+	#else:
+		#print("UR POOR")
 
-
+@onready var melee_pop_up = $"background/LittleGuyEconomy/shopbox2/melee minion/MeleePopup"
 func _on_melee_minion_button_down() -> void:
 	var cost = 10
 	if gold >= cost:
 		gold -= cost 
 		update_gold_display()
-		print("MELEE MINION x1")
+		
+		
+		melee_pop_up.text = "MELEE MINION x1"
+		melee_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		melee_pop_up.visible = false
 	else:
-		print("UR POOR")
-
-
-
-
-
+		melee_pop_up.text = "UR POOR"
+		melee_pop_up.visible = true
 		
+		await get_tree().create_timer(0.5).timeout
+		melee_pop_up.visible = false
 		
 
+@onready var skill_pop_up = $background/skills/VBoxContainer3/Buttonskill/SkillPopup
 
 func _on_buttonskill_button_down() -> void:
 	var cost = 6
 	if gold >= cost:
 		gold -= cost
 		update_gold_display()
-		print("SKILL")
+		
+		skill_pop_up.text = "SKILL"
+		skill_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		skill_pop_up.visible = false
 	else:
-		print("UR POOR")
+		skill_pop_up.text = "UR POOR"
+		skill_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		skill_pop_up.visible = false
+	
+@onready var ult_pop_up = $"background/skills/VBoxContainer2/buttonult/UltPopup"
 
 func _on_buttonult_button_down() -> void:
 	var cost = 12
 	if gold >= cost:
 		gold -= cost
 		update_gold_display()
-		print("ULT")
+	
+		ult_pop_up.text = "ULT"
+		ult_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		ult_pop_up.visible = false
 	else:
-		print("UR POOR")
+		ult_pop_up.text = "UR POOR"
+		ult_pop_up.visible = true
+		
+		await get_tree().create_timer(0.5).timeout
+		ult_pop_up.visible = false
+
+	
+
+
+	
